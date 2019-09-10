8d8836229eefa83ada1de24602b7462e0ac270bd
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-09-10 21:40:48 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 鈴木裕奈 on 2019/09/10.
+//  Copyright © 2019 Yuna Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
